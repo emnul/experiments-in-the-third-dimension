0ca4a840dc473a2f3dc22d88d4c0d5c5e977fecc
emanuel-sol <esolis6114@gmail.com>
2023-11-12 19:14:20 -0500

    init template

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+export default {
+  plugins: {
+    tailwindcss: { config: "./tailwindcss-config.ts" },
+    autoprefixer: {},
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { TextureLoader }  from 'three/src/loaders/TextureLoader.js'
+import { EquirectangularReflectionMapping } from 'three/src/constants.js'
+import { Suspense } from 'react'
+
+
+function App() {
+  const [envMap] = useLoader(TextureLoader, ['public/blockadesLabsSkybox/alien-cave.jpg'])
+  envMap.mapping = EquirectangularReflectionMapping
+
+  return (
+    <>
+      <div id="canvas-container" className='fixed top-0 left-0 w-full h-full overflow-hidden'>
+        <Canvas scene={{background: envMap, environment: envMap}}>
+          <ambientLight intensity={0.1} />
+          <Suspense fallback={null}>
+            <mesh>
+              <sphereGeometry />
+              <shaderMaterial  />
+            </mesh>
+          </Suspense>
+        </Canvas>
+      </div>
+    </>
+  )
+}
+
+export default App
