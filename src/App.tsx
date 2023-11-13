05f7cd9fc1b0278f5ec41aef16bf2a41b72742aa
emanuel-sol <esolis6114@gmail.com>
2023-11-12 19:46:51 -0500

    prettier formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
-import { Canvas, useFrame, useLoader } from '@react-three/fiber'
-import { TextureLoader }  from 'three/src/loaders/TextureLoader.js'
-import { EquirectangularReflectionMapping } from 'three/src/constants.js'
-import { Suspense } from 'react'
-
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three/src/loaders/TextureLoader.js";
+import { EquirectangularReflectionMapping } from "three/src/constants.js";
+import { Suspense } from "react";
 
 function App() {
-  const [envMap] = useLoader(TextureLoader, ['public/blockadesLabsSkybox/alien-cave.jpg'])
-  envMap.mapping = EquirectangularReflectionMapping
+  const [envMap] = useLoader(TextureLoader, [
+    "public/blockadesLabsSkybox/alien-cave.jpg",
+  ]);
+  envMap.mapping = EquirectangularReflectionMapping;
 
   return (
     <>
-      <div id="canvas-container" className='fixed top-0 left-0 w-full h-full overflow-hidden'>
-        <Canvas scene={{background: envMap, environment: envMap}}>
+      <div
+        id="canvas-container"
+        className="fixed top-0 left-0 w-full h-full overflow-hidden"
+      >
+        <Canvas scene={{ background: envMap, environment: envMap }}>
           <ambientLight intensity={0.1} />
           <Suspense fallback={null}>
             <mesh>
               <sphereGeometry />
-              <shaderMaterial  />
+              <shaderMaterial />
             </mesh>
           </Suspense>
         </Canvas>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
