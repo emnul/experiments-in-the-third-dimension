4e74fc470e064eed45586c7bf281692890e9280b
emanuel-sol <esolis6114@gmail.com>
2023-11-15 16:17:01 -0500

    Add debug flag

--- src/components/WebGLExperiment.tsx
+++ src/components/WebGLExperiment.tsx
@@ -2,11 +2,11 @@ import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Perf } from "r3f-perf";
 
-function WebGLExperiment() {
+function WebGLExperiment({ debug = false }) {
   return (
     <div id="canvas-container" className="canvas-container">
       <Canvas>
-        <Perf />
+        {debug ? <Perf /> : <></>}
         <gridHelper />
         <pointLight position={[10, 10, 10]} />
         <ambientLight intensity={0.1} />
