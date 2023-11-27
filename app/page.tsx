f1051321ad925aa472250bd869f104f47dd49d95
emanuel-sol <esolis6114@gmail.com>
2023-11-26 22:10:07 -0500

    Integrate Leva with OrganicSphere

--- app/page.tsx
+++ app/page.tsx
@@ -4,35 +4,71 @@ import vertexShader from "@/shaders/OrganicSphere/vertex.glsl";
 import fragmentShader from "@/shaders/OrganicSphere/fragment.glsl";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { useRef } from "react";
+import { useMemo, useRef } from "react";
 import { Color, ShaderMaterial, Vector2 } from "three";
 import styles from "../components/ShaderExperiment/shaderExperiment.module.css";
 import Script from "next/script";
+import { v4 as uuidv4 } from "uuid";
+import { useControls } from "leva";
 
 const OrganicSphere = () => {
-  const shaderRef = useRef<ShaderMaterial>(null!);
+  const organicSphereMaterialRef = useRef<ShaderMaterial>(null!);
   const { viewport } = useThree();
   const resolution = new Vector2();
+  const {
+    controlDistortionFrequency,
+    controlDistortionStrength,
+    controlDisplacementFrequency,
+    controlDisplacementStrength,
+    controlTimeFrequency,
+  } = useControls({
+    controlDistortionFrequency: 2.0,
+    controlDistortionStrength: 1.0,
+    controlDisplacementFrequency: 3.0,
+    controlDisplacementStrength: 0.2,
+    controlTimeFrequency: 0.1,
+  });
+
+  const uniforms = useMemo(
+    () => ({
+      uTime: { value: 0.0 },
+      uResolution: { value: resolution },
+      uDistortionFrequency: { value: 2.0 },
+      uDistortionStrength: { value: 1.0 },
+      uDisplacementFrequency: { value: 3.0 },
+      uDisplacementStrength: { value: 0.2 },
+      uTimeFrequency: { value: 0.1 },
+    }),
+    []
+  );
 
   useFrame((state, delta) => {
-    shaderRef.current.uniforms.uTime.value += delta;
+    organicSphereMaterialRef.current.uniforms.uTime.value += delta;
 
-    shaderRef.current.uniforms.uResolution.value = resolution.set(
-      state.size.width * viewport.dpr,
-      state.size.height * viewport.dpr
-    );
+    organicSphereMaterialRef.current.uniforms.uResolution.value =
+      resolution.set(
+        state.size.width * viewport.dpr,
+        state.size.height * viewport.dpr
+      );
+
+    organicSphereMaterialRef.current.uniforms.uDistortionFrequency.value =
+      controlDistortionFrequency;
+    organicSphereMaterialRef.current.uniforms.uDistortionStrength.value =
+      controlDistortionStrength;
+    organicSphereMaterialRef.current.uniforms.uDisplacementFrequency.value =
+      controlDisplacementFrequency;
+    organicSphereMaterialRef.current.uniforms.uDisplacementStrength.value =
+      controlDisplacementStrength;
+    organicSphereMaterialRef.current.uniforms.uTimeFrequency.value =
+      controlTimeFrequency;
   });
   return (
     <mesh>
       <sphereGeometry args={[1, 512, 512]} />
       <shaderMaterial
-        ref={shaderRef}
-        uniforms={{
-          uTime: { value: 0.0 },
-          uResolution: {
-            value: resolution,
-          },
-        }}
+        key={uuidv4()}
+        ref={organicSphereMaterialRef}
+        uniforms={uniforms}
         vertexShader={resolveLygia(vertexShader)}
         fragmentShader={resolveLygia(fragmentShader)}
       />
@@ -43,7 +79,7 @@ const OrganicSphere = () => {
 function OrganicSphereScene() {
   return (
     <main className={styles.canvasContainer}>
-      <Canvas scene={{ background: new Color("black") }}>
+      <Canvas dpr={[1, 2]} scene={{ background: new Color("black") }}>
         <OrganicSphere />
         <OrbitControls />
       </Canvas>
--- shaders/OrganicSphere/vertex.glsl
+++ shaders/OrganicSphere/vertex.glsl
@@ -1,4 +1,9 @@
 uniform float uTime;
+uniform float uDistortionFrequency;
+uniform float uDistortionStrength;
+uniform float uDisplacementFrequency;
+uniform float uDisplacementStrength;
+uniform float uTimeFrequency;
 
 varying vec3 vNormal;
 varying float vPerlinStrength;
@@ -7,16 +12,10 @@ varying float vPerlinStrength;
 
 
 void main() {
-  float uDistortionFrequency = 2.0;
-  float uDistortionStrength = 1.0;
-  float uDisplacementFrequency = 3.0;
-  float uDisplacementStrength = 0.2;
-
-
   vec3 displacementPosition = position;
-  displacementPosition += cnoise(vec4(displacementPosition * uDistortionFrequency, uTime * 0.1)) * uDistortionStrength;
+  displacementPosition += cnoise(vec4(displacementPosition * uDistortionFrequency, uTime * uTimeFrequency)) * uDistortionStrength;
 
-  float perlinStrength = cnoise(vec4(displacementPosition * uDisplacementFrequency, uTime * 0.1)) * uDisplacementStrength;
+  float perlinStrength = cnoise(vec4(displacementPosition * uDisplacementFrequency, uTime * uTimeFrequency)) * uDisplacementStrength;
   vec3 newPosition = position;
   newPosition += normal * perlinStrength;
 
